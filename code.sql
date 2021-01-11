7c32c04333bbc7801b3a5dea96a7a16b73ce0f90
Stephen Titcombe <40187876+stitcombe@users.noreply.github.com>
2021-01-11 12:56:05 -0600

    Update code.sql

--- code.sql
+++ code.sql
@@ -1,82 +1,66 @@
 SELECT DISTINCT 
-ISNULL(d.ClientValue, 'No Client Selected') AS Client,
-e.COUNT_ReqRcvd,
-f.COUNT_ReqClosed,
-g.COUNT_Touched,
-h.COUNT_DetailRcvd,
-i.COUNT_DtlReqDue,
-j.COUNT_DtlReqComplete
+ISNULL(d.ClientValue, 'No Client') AS Client,
+e.COUNT_Type1 AS "Count Type 1",
+f.COUNT_Type2 AS "Count Type 2",
+g.COUNT_Type3 AS "Count Type 3",
+h.COUNT_Type4 AS "Count Type 4",
+i.COUNT_Type5 AS "Count Type 5",
+j.COUNT_Type6 AS "Count Type 6"
 
 FROM vw_InfoClients d
 
 LEFT JOIN (
-SELECT ISNULL(a.Client,'No Client Selected') AS Client, COUNT(DISTINCT a.REQID) AS COUNT_ReqRcvd
+SELECT ISNULL(a.Client,'No Client') AS Client, COUNT(DISTINCT a.REQID) AS COUNT_Type1
 FROM dbo.vw_ReqSearchResult a
 LEFT JOIN dbo.vw_Requests b ON b.ReqID = a.REQID
 LEFT JOIN dbo.vw_DetailReq c ON c.ReqID = a.REQID
-WHERE a.ReqStatus <> 'Cancelled'
-AND b.ReqDateReceived between 'yyyy-MM-dd' and 'yyyy-MM-dd'
 GROUP BY a.Client
-) e ON ISNULL(d.ClientValue, 'No Client Selected') = e.Client 
+) e ON ISNULL(d.ClientValue, 'No Client') = e.Client 
 
 LEFT JOIN (
-SELECT ISNULL(a.Client,'No Client Selected') AS Client, COUNT(DISTINCT a.REQID) AS COUNT_ReqClosed
+SELECT ISNULL(a.Client,'No Client') AS Client, COUNT(DISTINCT a.REQID) AS COUNT_Type2
 FROM dbo.vw_ReqSearchResult a
 LEFT JOIN dbo.vw_Requests b ON b.ReqID = a.REQID
 LEFT JOIN dbo.vw_DetailReq c ON c.ReqID = a.REQID
-WHERE a.ReqStatus <> 'Cancelled'
-AND b.ReqClosedDate between 'yyyy-MM-dd' and 'yyyy-MM-dd'
 GROUP BY a.Client
-) f ON ISNULL(d.ClientValue, 'No Client Selected') = f.Client
+) f ON ISNULL(d.ClientValue, 'No Client') = f.Client
 
 LEFT JOIN (
-SELECT ISNULL(a.Client,'No Client Selected') AS Client, COUNT(DISTINCT a.REQID) AS COUNT_Touched
+SELECT ISNULL(a.Client,'No Client') AS Client, COUNT(DISTINCT a.REQID) AS COUNT_Type3
 FROM dbo.vw_ReqSearchResult a
 LEFT JOIN dbo.vw_Requests b ON b.ReqID = a.REQID
 LEFT JOIN dbo.vw_DetailReq c ON c.ReqID = a.REQID
-WHERE a.ReqStatus <> 'Cancelled'
-AND (b.ReqDateReceived between 'yyyy-MM-dd' and 'yyyy-MM-dd'
-OR b.ReqClosedDate between 'yyyy-MM-dd' and 'yyyy-MM-dd'
-OR b.ReqDateReceived between 'yyyy-MM-dd' and 'yyyy-MM-dd'
-OR c.ReceivedDate between 'yyyy-MM-dd' and 'yyyy-MM-dd'
-OR c.CompletedDate between 'yyyy-MM-dd' and 'yyyy-MM-dd')
 GROUP BY a.Client
-) g ON ISNULL(d.ClientValue, 'No Client Selected') = g.Client
+) g ON ISNULL(d.ClientValue, 'No Client') = g.Client
 
 LEFT JOIN (
-SELECT ISNULL(a.Client,'No Client Selected') AS Client, COUNT(DISTINCT a.REQID) AS COUNT_DetailRcvd
+SELECT ISNULL(a.Client,'No Client') AS Client, COUNT(DISTINCT a.REQID) AS COUNT_Type4
 FROM dbo.vw_ReqSearchResult a
 LEFT JOIN dbo.vw_Requests b ON b.ReqID = a.REQID
 LEFT JOIN dbo.vw_DetailReq c ON c.ReqID = a.REQID
-WHERE a.ReqStatus <> 'Cancelled'
-AND c.ReceivedDate between 'yyyy-MM-dd' and 'yyyy-MM-dd'
 GROUP BY a.Client
-) h ON ISNULL(d.ClientValue, 'No Client Selected') = h.Client
+) h ON ISNULL(d.ClientValue, 'No Client') = h.Client
 
 LEFT JOIN (
-SELECT ISNULL(a.Client,'No Client Selected') AS Client, COUNT(DISTINCT a.REQID) AS COUNT_DtlReqDue
+SELECT ISNULL(a.Client,'No Client') AS Client, COUNT(DISTINCT a.REQID) AS COUNT_Type5
 FROM dbo.vw_ReqSearchResult a
 LEFT JOIN dbo.vw_Requests b ON b.ReqID = a.REQID
 LEFT JOIN dbo.vw_DetailReq c ON c.ReqID = a.REQID
-WHERE a.ReqStatus <> 'Cancelled'
-AND c.DueDate between 'yyyy-MM-dd' and 'yyyy-MM-dd'
 GROUP BY a.Client
-) i ON ISNULL(d.ClientValue, 'No Client Selected') = i.Client
+) i ON ISNULL(d.ClientValue, 'No Client') = i.Client
 
 LEFT JOIN (
-SELECT ISNULL(a.Client,'No Client Selected') AS Client, COUNT(DISTINCT a.REQID) AS COUNT_DtlReqComplete
+SELECT ISNULL(a.Client,'No Client') AS Client, COUNT(DISTINCT a.REQID) AS COUNT_Type6
 FROM dbo.vw_ReqSearchResult a
 LEFT JOIN dbo.vw_Requests b ON b.ReqID = a.REQID
 LEFT JOIN dbo.vw_DetailReq c ON c.ReqID = a.REQID
-WHERE a.ReqStatus <> 'Cancelled'
-AND c.CompletedDate between 'yyyy-MM-dd' and 'yyyy-MM-dd'
 GROUP BY a.Client
-) j ON ISNULL(d.ClientValue, 'No Client Selected') = j.Client
+) j ON ISNULL(d.ClientValue, 'No Client') = j.Client
 
-WHERE COUNT_ReqRcvd is not null 
-OR COUNT_ReqClosed is not null
-OR COUNT_Touched is not null
-OR COUNT_DetailRcvd is not null
-OR COUNT_DtlReqDue is not null
-OR COUNT_DtlReqComplete is not null
+WHERE COUNT_Type1 is not null 
+OR COUNT_Type2 is not null
+OR COUNT_Type3 is not null
+OR COUNT_Type4 is not null
+OR COUNT_Type5 is not null
+OR COUNT_Type6 is not null
 ORDER BY Client ASC;
